8b6822cad4ce52690dc4f10a74e290c888bd4a97
Dan Loughlin <danloughlin@gmail.com>
2025-10-30 18:03:57 -0400

    Updates to startup and options files for naming consistency.

--- run_GLIMPSE_GCAM-USA-8.2-linux.sh
+++ run_GLIMPSE_GCAM-USA-8.2-linux.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # If the following command does not start the ScenarioBuilder, correct the JAVA_HOME path
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export JAVA_HOME="$SCRIPT_DIR/amazon-corretto-8.462.08.1-linux-x64"
+export JAVA_HOME="./amazon-corretto-8.462.08.1-linux-x64"
 
 if [ ! -f "$JAVA_HOME/bin/java" ]; then
   echo "JAVA_HOME setting needs to be fixed"
@@ -10,9 +9,9 @@ if [ ! -f "$JAVA_HOME/bin/java" ]; then
   exit 1
 fi
 
-export JAVA_JVM_PATH="$JAVA_HOME/bin/server"
+export JAVA_JVM_PATH="$JAVA_HOME/bin/server":"$JAVA_HOME/jre/bin"
 export PATH=".:$JAVA_JVM_PATH:$JAVA_HOME:$JAVA_HOME/bin:../../ModelInterface:$PATH"
 
-java -Dprism.order=sw -jar ./GLIMPSE-ScenarioBuilder/GLIMPSE-ScenarioBuilder.jar -options options_GCAM-global-8.2.txt
+java -Dprism.order=sw -jar ./GLIMPSE-ScenarioBuilder/GLIMPSE-ScenarioBuilder.jar -options options_GCAM-USA-8.2-linux.txt
 
 exit 0
